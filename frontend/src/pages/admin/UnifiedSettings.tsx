1e9ae400db56198166bf92bc2f284c5e2635cfbe
kareemelthird <kareemelthird@gmail.com>
2025-10-29 11:59:33 +0300

    Fix import path in UnifiedSettings.tsx - change ../lib/api to ../../lib/api

--- frontend/src/pages/admin/UnifiedSettings.tsx
+++ frontend/src/pages/admin/UnifiedSettings.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { api } from '../lib/api';
+import { api } from '../../lib/api';
 import {
   CogIcon,
   UserGroupIcon,
